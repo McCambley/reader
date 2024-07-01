f98ebc6cde8a894d6331c0433437af925ebc6491
McCambley <jakemccamb16@gmail.com>
2024-07-01 12:30:36 -0400

    Place refresh button closer to center of screen

--- src/components/RefreshButton.tsx
+++ src/components/RefreshButton.tsx
@@ -9,7 +9,7 @@ export const RefreshButton: React.FC = () => {
 
   return (
     <button
-      className="fixed bottom-4 right-4 rounded-lg bg-white p-1 min-w-8 min-h-8 filter grayscale"
+      className="fixed bottom-8 right-8 rounded-lg bg-white p-1 min-w-8 min-h-8 filter grayscale"
       onClick={onClick}
     >
       ♻️
