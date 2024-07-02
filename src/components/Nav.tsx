bdbf5fb04f88d0c9db25c48342de94110bfe596c
McCambley <jakemccamb16@gmail.com>
2024-07-02 10:06:29 -0400

    Underline /best nav link on homepage

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,18 +3,26 @@ import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const links = ["/best", "/new", "/read", "/random", "/all"];
+const LINKS = ["/best", "/new", "/read", "/random", "/all"];
+const DEFAULT_PATH = LINKS[0];
 
 export const Nav: React.FC = () => {
   const path = usePathname();
+  const onHomepage = path === "/";
+  const shouldUnderline = (link: string): boolean =>
+    path === link || (onHomepage && link === DEFAULT_PATH);
+
   return (
     <nav className="flex gap-2 items-end sticky top-0 bg-black z-10 pb-2">
       <h1 className="text-xl underline">
         <Link href="/">reader.</Link>
       </h1>
-      {links.map((link, index) => (
-        <Link key={links[index]} href={link}>
-          <span className={path === link ? "underline" : ""}> {link} </span>
+      {LINKS.map((link, index) => (
+        <Link key={link} href={link}>
+          <span className={shouldUnderline(link) ? "underline" : ""}>
+            {" "}
+            {link}{" "}
+          </span>
         </Link>
       ))}
     </nav>
