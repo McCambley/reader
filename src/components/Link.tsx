e9fc7e00759f5c4a2074343c2d584ce01effb49a
McCambley <jakemccamb16@gmail.com>
2024-07-05 01:03:28 -0400

    Refactor grid layout for simplicity

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -14,30 +14,6 @@ const parseLinkClass = (color: string | undefined): string => {
   }
 };
 
-const calculateSpacing = (
-  index: number,
-  importance: string | undefined
-): JSX.Element => {
-  const indexLength = Math.max(4, (index + 1).toString().length);
-  const importanceLength = importance ? importance.length : 0;
-  const absoluteLength = indexLength - importanceLength;
-
-  const spacing = "_".repeat(absoluteLength).length;
-
-  return (
-    <span
-      style={{
-        display: "inline-block",
-        minWidth: `${spacing}ch`,
-      }}
-    ></span>
-  );
-};
-
-const calculateNumberMargin = (index: number): number => {
-  return 4 - ((index + 1).toString().length + 1);
-};
-
 interface LinkProps {
   page: NotionDatabasePage;
   index: number;
@@ -49,8 +25,6 @@ export const Link: React.FC<LinkProps> = ({ page, index }) => {
   const facet = page.properties.Facet.select?.name;
   const url = page.properties.Link.url || page.url;
   const pageUrl = page.url;
-  // const source = page.properties.Source.select?.name;
-  // const checked = page.properties.Checked.checkbox;
   const name = page.properties.Name.title[0].plain_text;
   const shortenedUrl = url?.split("/")[2];
   const createdTime = new Date(
@@ -62,32 +36,21 @@ export const Link: React.FC<LinkProps> = ({ page, index }) => {
     day: "numeric",
   });
   const type = page.properties.Type.select?.name;
-  const spacing = calculateSpacing(index, importance);
-  // const tags = page.properties.Tags.multi_select.map((tag) => tag.name);
 
   return (
-    <li key={page.id} className="mb-3">
-      <a className={`${colorClass} flex`} href={url || ""}>
-        <span
-          className={`${colorClass} mr-[${calculateNumberMargin(index)}ch]`}
-        >
-          {index + 1}.
+    <li key={page.id} className="mb-3 grid grid-cols-[4ch_1fr]">
+      <p className={colorClass}>{index + 1}.</p>
+      <a className={colorClass} href={url || ""}>
+        <span className={colorClass}>
+          {name} <span className="opacity-50">({shortenedUrl})</span>
         </span>
-        <div>
-          <span className={colorClass}>{name}</span>
-          <span className="opacity-50">({shortenedUrl})</span>
-        </div>
       </a>
-      {/* <p className="italic text-xs"> */}
+      <p className="opacity-50">{importance ? importance : ""}</p>
       <p className="italic opacity-50 flex">
-        {importance ? <span className="not-italic">{importance}</span> : null}
-        {spacing}
         <a href={pageUrl || ""} className="">
           <span>{createdTime} · </span>
           {facet ? <span>{facet} </span> : null}
           {type ? <span>{type} </span> : null}
-          {/* {source ? <span>{source} </span> : null} */}
-          {/* {tags.length ? <span>{tags[0]} </span> : null} */}
         </a>
       </p>
     </li>
