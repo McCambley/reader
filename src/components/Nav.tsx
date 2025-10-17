da4f48ff1a09ac1c52f0e1388a1daaada8312a26
McCambley <jakemccamb16@gmail.com>
2025-10-17 12:05:44 -0400

    refactor: simplify JSX structure in Nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -16,35 +16,33 @@ export const Nav: React.FC = () => {
     );
   };
   return (
-    <>
-      <nav className="sticky top-0 z-10 flex justify-between w-full">
-        <div>
-          <div className="text-xl flex gap-2 items-end pb-1">
-            <h1 className="text-xl">
-              <Link href="/" className="">
-                reader.
-              </Link>
-            </h1>
-            {LINKS.map((link, index) => (
-              <Link key={link} href={link} className="opacity-50">
-                <span>{link.slice(0, 1)}</span>
-                <span className={shouldUnderline(link) ? "underline" : ""}>
-                  {LINK_ALIASES[link] ? LINK_ALIASES[link] : link.slice(1, 2)}
-                </span>
-              </Link>
-            ))}
-          </div>
-          <p className="pb-2 opacity-50">
-            {path?.length === 1 ? DEFAULT_PATH : path}
-          </p>
+    <nav className="sticky top-0 z-10 flex justify-between w-full">
+      <div>
+        <div className="text-xl flex gap-2 items-end pb-1">
+          <h1 className="text-xl">
+            <Link href="/" className="">
+              reader.
+            </Link>
+          </h1>
+          {LINKS.map((link, index) => (
+            <Link key={link} href={link} className="opacity-50">
+              <span>{link.slice(0, 1)}</span>
+              <span className={shouldUnderline(link) ? "underline" : ""}>
+                {LINK_ALIASES[link] ? LINK_ALIASES[link] : link.slice(1, 2)}
+              </span>
+            </Link>
+          ))}
         </div>
-        <button
-          className="help-button text-xl rounded-full p-1 "
-          onClick={handleHelpClick}
-        >
-          ?
-        </button>
-      </nav>
-    </>
+        <p className="pb-2 opacity-50">
+          {path?.length === 1 ? DEFAULT_PATH : path}
+        </p>
+      </div>
+      <button
+        className="help-button text-xl rounded-full p-1 "
+        onClick={handleHelpClick}
+      >
+        ?
+      </button>
+    </nav>
   );
 };
