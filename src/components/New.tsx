2d5d55198ca22e657113e89c3b88298129f8de46
McCambley <jakemccamb16@gmail.com>
2025-10-22 11:36:27 -0400

    feat: enhance data fetching in New component with sorting and limiting options

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -3,6 +3,14 @@ import { getNotionData } from "@/utils/getNotionData";
 import { Links } from "@/components/Links";
 
 export async function New() {
-  const results = await getNotionData({});
+  const results = await getNotionData({
+    limit: true,
+    sorts: [
+      {
+        property: "Created time",
+        direction: "descending",
+      },
+    ],
+  });
   return <Links pages={results} />;
 }
